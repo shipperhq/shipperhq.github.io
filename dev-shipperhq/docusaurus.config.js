676d527eb6d161f66458e659c1b4e0de06c80036
Rachel Chhay <chhay.rachel@gmail.com>
2022-02-28 17:35:31 -0600

    RIV-950 feat: add sass to project

--- dev-shipperhq/docusaurus.config.js
+++ dev-shipperhq/docusaurus.config.js
@@ -58,6 +58,7 @@ const config = {
         }
       },
     ],
+    'docusaurus-plugin-sass'
   ],
 
   themeConfig:
--- dev-shipperhq/src/pages/index.js
+++ dev-shipperhq/src/pages/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
+import styles from './index.module.scss';
 import HomepageFeatures from '../components/HomepageFeatures';
+import './homepage.scss'
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
+        <h1 className="hero__title test">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <h2>
