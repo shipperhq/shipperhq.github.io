5baf1f226cd580d5fcb2f6fb944d18be7dd6a59f
Rachel Chhay <chhay.rachel@gmail.com>
2022-03-03 14:21:18 -0600

    RIV-961 create contact form and add HubSpot form

---
+++ dev-shipperhq/src/pages/contact.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { HubSpotForm } from '../components/HubSpotForm';
+import Layout from '@theme/Layout';
+import './contact-us.scss';
+
+function Contact() {
+  return (
+    <Layout>
+      <div className="container">
+        <h1 className="contact-title">Contact Us</h1>
+
+        <div className="contact-form">
+          <HubSpotForm portalId="20068459" formId="f34e690f-a952-4cb0-90d0-522c03304dd9" />
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export default Contact
